3b5321386e29c16206321d5cc2c4f13f6eb8ca65
ghdtjgus76 <ghdtjgus76@naver.com>
2023-06-26 18:03:01 +0900

    Feat(Discount): Add RateDiscountPolicy and RateDiscountPolicy Test

---
+++ src/main/java/ghdtjgus76/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package ghdtjgus76.core.discount;
+
+import ghdtjgus76.core.member.Grade;
+import ghdtjgus76.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
