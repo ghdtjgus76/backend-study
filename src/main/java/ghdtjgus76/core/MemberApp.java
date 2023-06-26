ccda23b8aac2f291e7459d70af094a01a605b8b9
ghdtjgus76 <ghdtjgus76@naver.com>
2023-06-27 01:15:22 +0900

    Refactor: Refactor codes from java to spring

--- src/main/java/ghdtjgus76/core/MemberApp.java
+++ src/main/java/ghdtjgus76/core/MemberApp.java
@@ -4,11 +4,17 @@ import ghdtjgus76.core.member.Grade;
 import ghdtjgus76.core.member.Member;
 import ghdtjgus76.core.member.MemberService;
 import ghdtjgus76.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
 //        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
