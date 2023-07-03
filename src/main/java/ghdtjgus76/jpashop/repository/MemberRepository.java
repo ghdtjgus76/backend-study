8c3b95bec7fad689de8c2d701fc1eb159b08cc21
ghdtjgus76 <ghdtjgus76@naver.com>
2023-07-03 23:17:44 +0900

    Refactor(Repository): Refactor dependency injection

--- src/main/java/ghdtjgus76/jpashop/repository/MemberRepository.java
+++ src/main/java/ghdtjgus76/jpashop/repository/MemberRepository.java
@@ -3,14 +3,15 @@ package ghdtjgus76.jpashop.repository;
 import ghdtjgus76.jpashop.domain.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
