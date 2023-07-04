658d93a17024b1346b6bc5ce5d6300c815e0ea07
ghdtjgus76 <ghdtjgus76@naver.com>
2023-07-04 18:33:13 +0900

    Feat(Service): Add ItemService

---
+++ src/main/java/ghdtjgus76/jpashop/service/ItemService.java
@@ -0,0 +1,27 @@
+package ghdtjgus76.jpashop.service;
+
+import ghdtjgus76.jpashop.domain.item.Item;
+import ghdtjgus76.jpashop.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
