db8b16cf685b35a196bd6f5d2a3d57ed228f326d
ghdtjgus76 <ghdtjgus76@naver.com>
2023-07-07 14:16:56 +0900

    Feat: Add item order feature and page

---
+++ src/main/java/ghdtjgus76/jpashop/controller/OrderController.java
@@ -0,0 +1,43 @@
+package ghdtjgus76.jpashop.controller;
+
+import ghdtjgus76.jpashop.domain.Member;
+import ghdtjgus76.jpashop.domain.item.Item;
+import ghdtjgus76.jpashop.service.ItemService;
+import ghdtjgus76.jpashop.service.MemberService;
+import ghdtjgus76.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+
+        return "redirect:/orders";
+    }
+}
